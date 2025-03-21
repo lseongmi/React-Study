987b67f787c1704b461587c511c319c9509c15ee
lseongmi <w2307@e-mirim.hs.kr>
2025-03-21 14:32:30 +0900

    Movie 리액트 실습

---
+++ src/component/Movie.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} />
+      <h2>{title}</h2>
+      <p>
+        {summary.trim().length === 0
+          ? "영화 요약이 존재하지 않습니다."
+          : summary}
+      </p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
---
+++ src/routes/Home.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import Movie from "../component/Movie";
+
+function Home() {
+  const [loding, setLoding] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoding(false);
+  };
+  console.log(movies);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loding ? (
+        <h1>Loding...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
